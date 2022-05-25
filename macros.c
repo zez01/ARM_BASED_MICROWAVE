47cffac5df99b825bba306d7fd935112cc03e696
zyady <1901108@eng.asu.edu.eg>
2022-05-25 22:43:46 +0200

    To include all files in main

---
+++ macros.c
@@ -0,0 +1,12 @@
+void Check_door();
+unsigned char P___S();
+void count_down_edit(int x,int y);
+void count_delay_sec(float x);
+void ItoA( float num,unsigned char * str );
+int atoi(unsigned char* str);
+void string_shift(unsigned char* str ,int k );
+unsigned char getkey_real_modify(char*to_print);
+unsigned char getkey_real();
+void init_portF();
+void init_portA();
+

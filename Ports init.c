9296131859cdacb1641b34523cda3b6910c61d4f
Keyrollos <105951752+Keyrollos@users.noreply.github.com>
2022-05-25 18:25:46 +0200

    Update Ports init.c

--- Ports init.c
+++ Ports init.c
@@ -11,16 +11,3 @@ void init_portF(){
   GPIO_PORTF_PUR_R=0x11;
   GPIO_PORTF_DATA_R =0x00 ;
 }
-void init_portA(){
-  SYSCTL_RCGCGPIO_R |=0x01;
-  while ((SYSCTL_PRGPIO_R&0x01) == 0){};
-  GPIO_PORTA_LOCK_R= 0x4C4F434B;
-    GPIO_PORTA_CR_R|=0x0F; //portA 2, 3 
-  GPIO_PORTA_AFSEL_R&=~0xf;
-  GPIO_PORTA_PCTL_R&=~0xf;
-  GPIO_PORTA_AMSEL_R&=~0xf;
-  GPIO_PORTA_DIR_R|=0x08; //PORT a0=out 
-  GPIO_PORTA_DEN_R|=0x0F;
-  GPIO_PORTA_PUR_R|=0x0F;
-  GPIO_PORTA_DATA_R &=~0xf ;
-}
